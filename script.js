df83c44debe313b5d02b0a7c0b13b77879098238
Shekhar Singh <smehla147@gmail.com>
2024-05-12 12:11:24 +1000

    added the switching task functionality

--- script.js
+++ script.js
@@ -2,21 +2,58 @@ let objectList = [];
 
 const submitHandler = (e) => {
   const newForm = new FormData(e);
-  const task = newForm.get("task");
-  const hour = newForm.get("hours");
+  let task = newForm.get("task");
+  let hour = newForm.get("hours");
   const obj = {
     task,
     hour,
     id: idgenrator(),
+    type: "good",
   };
   objectList.push(obj);
-  display();
+  displayGoodList();
 };
-// this function is responsible to dispaly data in table dynamically.
-const display = () => {
+// this function is responsible to dispaly data in entry  table dynamically.
+const displayGoodList = () => {
+  const displayentlist = document.querySelector("#entrylist");
+
   let strToDisplay = "";
-  objectList.map((item) => {
-    strToDisplay += `
+  objectList = objectList.filter((item) => {
+    return item.type === "good";
+  });
+  objectList.map((item, i) => {
+    const str = `
+    
+    <td scope="row">${i + 1}</td>
+    <td>${item.hour}</td>
+    <td>${item.task}</td>
+    <td class="text-end">
+    <button class="btn btn-danger deletebutton mx-2"  onclick = 'ondelete("${
+      item.id
+    }")' >
+    <i class="fa-solid fa-trash"></i></button
+    ><button class="btn btn-success "   onclick = 'switchtask("${
+      item.id
+    }","bad")'>
+    <i class="fa-solid fa-arrow-right"></i>
+    </button>
+    </td>
+    </tr>`;
+    strToDisplay += str;
+  });
+  displayentlist.innerHTML = strToDisplay;
+};
+// this function is responsible to dispaly data in entry  table dynamically.
+const displaybadList = () => {
+  const displaybadlist = document.querySelector("#badlist");
+
+  let strToDisplay = "";
+  objectList = objectList.filter((item) => {
+    return item.type === "bad";
+  });
+
+  objectList.map((item, i) => {
+    const str = `
 
   <td scope="row">${i + 1}</td>
   <td>${item.hour}</td>
@@ -24,17 +61,20 @@ const display = () => {
   <td class="text-end">
     <button class="btn btn-danger deletebutton mx-2"  onclick = 'ondelete("${
       item.id
-    }")' >
+    }", "bad")' >
       <i class="fa-solid fa-trash"></i></button
-    ><button class="btn btn-success">
+    ><button class="btn btn-success "   onclick = 'switchtask("${
+      item.id
+    }","good")'>
       <i class="fa-solid fa-arrow-right"></i>
     </button>
   </td>
   </tr>`;
-    const displayentlist = document.querySelector("#entrylist");
-    return (displayentlist.innerHTML = strToDisplay);
+    strToDisplay += str;
   });
+  displaybadlist.innerHTML = strToDisplay;
 };
+
 // id gentrator
 function idgenrator() {
   const str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890";
@@ -48,9 +88,19 @@ function idgenrator() {
 
 // // handle the deletebutton
 const ondelete = (id) => {
-  const newObjectList = objectList.filter((item) => {
+  objectList = objectList.filter((item) => {
     return item.id !== id;
   });
-  objectList = newObjectList;
-  display();
+  displayGoodList();
+};
+const switchtask = (id, type) => {
+  objectList = objectList.map((item) => {
+    if (item.id === id) {
+      item.type = type;
+    }
+    return item;
+  });
+
+  displaybadList();
+  displaybadList();
 };
